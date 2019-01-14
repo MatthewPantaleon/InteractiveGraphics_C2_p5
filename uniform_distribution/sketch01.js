d9926f692d6becc18bbb5717b668d80407cafbec
MatthewPantaleon <45208898+MatthewPantaleon@users.noreply.github.com>
2019-01-14 15:29:06 +0000

    Uniform Distribution

---
+++ uniform_distribution/sketch01.js
@@ -0,0 +1,34 @@
+var walker;
+
+var counts = [];
+var totalCounts = 20;
+
+function setup(){
+	createCanvas(500, 500);
+	background(127);
+	
+	for(var i = 0; i < totalCounts; i++){
+		counts[i] = 0;
+	}
+	
+}
+
+
+
+function draw(){
+	
+	var selectRandomIndex = floor(random(totalCounts));
+	counts[selectRandomIndex]++;
+	
+	noStroke();
+	fill(0, 220, 0);
+	
+	var barWidth = width / counts.length;
+	
+	for(var x = 0; x < counts.length; x++){
+		rect(x * barWidth, height - counts[x], barWidth - 5, 1);
+	}
+	
+}
+
+	
